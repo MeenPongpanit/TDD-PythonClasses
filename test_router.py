da2dacab13162e8421c0d2a325ab5e91c22431a7
MeenPongpanit <61070124@kmitl.ac.th>
2021-02-09 14:30:08 +0700

    Update test_router.py

--- test_router.py
+++ test_router.py
@@ -10,9 +10,19 @@ def test_named_router():
     router1 = Router.Router(name)
     assert router1.name == name, "test failed"
 
+def test_fullinfo_router():
+    name = "Test Name"
+    model = "Test Model"
+    vendor = "Test Vendor"
+    router1 = Router.Router(name, model, vendor)
+    assert router1.name == name, 'test failed'
+    assert router1.model == model, 'test failed'
+    assert router1.vendor == vendor, 'test failed'
+
 test_list = [
     test_create_router,
-    test_named_router
+    test_named_router,
+    test_fullinfo_router
 ]
 
 for test in test_list:
