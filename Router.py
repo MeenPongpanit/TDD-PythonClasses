628e2ccd7061d782602c36e81d786a22cfb14b5b
MeenPongpanit <61070124@kmitl.ac.th>
2021-02-09 14:31:10 +0700

    fully info create router

--- Router.py
+++ Router.py
@@ -1,5 +1,7 @@
 """Router"""
 
 class Router():
-    def __init__(self, name="R0"):
+    def __init__(self, name=None, model=None, vendor=None):
         self.name = name
+        self.model = model
+        self.vendor = vendor
